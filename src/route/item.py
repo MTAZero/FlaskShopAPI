defabff64d3f872c6a563b8e30e4bf7847d8442f
MTAZero <buithuy3996@gmail.com>
2019-02-28 02:18:12 +0900

    save

--- src/route/item.py
+++ src/route/item.py
@@ -4,21 +4,6 @@ from bson.json_util import dumps
 
 from model import item
 
-# ans = [
-#     {
-#         "name": u"Macbook Air",
-#         "price": 2500
-#     },
-#     {
-#         "name": u"Iphone XS MAX",
-#         "price": 1500
-#     },
-#     {
-#         "name": u"Samsung note 9",
-#         "price": 1500
-#     }
-# ]
-
 item_blueprint = Blueprint('item', __name__)
 item_blueprint_api = Api(item_blueprint)
 
